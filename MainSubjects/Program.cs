963bf7e061589fa3d6221b60638099ae21277a3c
Alperen İNEL <inelalperen@gmail.com>
2024-10-28 18:00:48 +0300

    Add project files.

---
+++ MainSubjects/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region PrintCommend
+
+            Console.Write("Hello");
+            Console.WriteLine("Hello World");
+
+
+            Console.WriteLine(">>> Restaurant Menu <<<");
+            Console.WriteLine();
+            Console.WriteLine("1-Soups");
+            Console.WriteLine("2-Main Dishes");
+            Console.WriteLine("3-Entrees");
+            Console.WriteLine("4-Salads");
+            Console.WriteLine("5-Desserts");
+            Console.WriteLine("6-Drinks");
+
+
+            #endregion
+
+            #region StringVariables
+
+            string customerName;
+            string customerSurname;
+            string customerPhone;
+            string customerEmail, customerDistrict, customerCity;
+
+            customerName = "Alperen";
+            customerSurname = "İnel";
+            customerPhone = "+90 555 444 33 22";
+            customerEmail = "mymailaddress@gmail.com";
+            customerDistrict = "Greenwich";
+            customerCity = "London";
+
+            Console.WriteLine(">>> Reservation Card <<<");
+            Console.WriteLine();
+            Console.WriteLine("-------------------------------------------------");
+            Console.WriteLine("Customer: " + customerName + " " + customerSurname);
+            Console.WriteLine("Communication: " + customerPhone);
+            Console.WriteLine("Email Address: " + customerEmail);
+            Console.WriteLine("Addres: " + customerDistrict + "/" + customerCity);
+            Console.WriteLine("-------------------------------------------------");
+
+            #endregion
+
+            #region IntVariables
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("**** Restaurant Menu Prices ****");
+            Console.WriteLine();
+            Console.WriteLine("-----Hamburger: " + hamburgerPrice + " TL");
+            Console.WriteLine("-----Pizza: " + pizzaPrice + " TL");
+            Console.WriteLine("-----Cola: " + cokePrice + " TL");
+            Console.WriteLine("-----Limonade: " + lemonadePrice + " TL");
+            Console.WriteLine("-----Fries: " + friesPrice + " TL");
+            Console.WriteLine("-----Water: " + waterPrice + " TL");
+
+            Console.WriteLine();
+            int hamburgerCount;
+            int cokeCount;
+            int waterCount;
+            int friesCount;
+            int pizzaCount;
+            int lemonadeCount;
+
+            int totalHamgurgerPrice;
+            int totalCokePrice;
+            int totalWaterPrice;
+            int totalFriesPrice;
+            int totalPizzaPrice;
+            int totalLemonadePrice;
+
+            hamburgerCount = 3;
+            cokeCount = 3;
+            waterCount = 3;
+            friesCount = 1;
+            pizzaCount = 0;
+            lemonadeCount = 0;
+
+            totalHamgurgerPrice = hamburgerCount * hamburgerPrice;
+            totalCokePrice = cokeCount * cokePrice;
+            totalWaterPrice = waterCount * waterPrice;
+            totalLemonadePrice = lemonadeCount * lemonadePrice;
+            totalFriesPrice = friesCount * friesPrice;
+            totalPizzaPrice = pizzaCount * pizzaPrice;
+
+            Console.WriteLine("-------------------------------------");
+            Console.WriteLine("Hamburger Price: " + totalHamgurgerPrice + " TL");
+            Console.WriteLine("Pizza Price: " + totalPizzaPrice + " TL");
+            Console.WriteLine("Fries Price: " + totalFriesPrice + " TL");
+            Console.WriteLine("Cola Price: " + totalCokePrice + " TL");
+            Console.WriteLine("Limonade Price: " + totalLemonadePrice + " TL");
+            Console.WriteLine("Water Price: " + totalWaterPrice + " TL");
+
+            Console.WriteLine();
+
+            int totalPrice = totalCokePrice + totalWaterPrice + totalLemonadePrice + totalHamgurgerPrice + totalPizzaPrice + totalFriesPrice;
+
+            Console.WriteLine("Total Price: " + totalPrice + " TL");
+
+            #endregion
+            Console.Read();
+        }
+    }
+}
---
+++ Variables/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region DoubleVariables
+
+            double number;
+            number = 3.7;
+            Console.WriteLine(number);
+
+            Console.WriteLine(">>> Price List <<<");
+            Console.WriteLine();
+
+            double applePrice, orangePrice, strawberryPrice, potatoPrice, tomatoPrice;
+
+            applePrice = 10.50;
+            orangePrice = 20.75;
+            strawberryPrice = 100;
+            potatoPrice = 6.12;
+            tomatoPrice = 26.33;
+
+            Console.WriteLine("---- Apple Price (kg) :" + applePrice + " TL");
+            Console.WriteLine("---- Orange Price (kg):" + orangePrice + " TL");
+            Console.WriteLine("---- Strawberry Price (kg):" + strawberryPrice + " TL");
+            Console.WriteLine("---- Patato Price (kg):" + potatoPrice + " TL");
+            Console.WriteLine("---- Tomato Price (kg):" + tomatoPrice + " TL");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            double appleGram, orangeGram, strawberryGram, potatoGram, tomatoGram;
+
+            appleGram = 1.245;
+            orangeGram = 2.650;
+            strawberryGram = 0.750;
+            potatoGram = 4.859;
+            tomatoGram = 3.745;
+
+            double appleTotalPrice = appleGram * applePrice;
+            double orangeTotalPrice = orangeGram * orangePrice;
+            double strawberryTotalPrice = strawberryGram * strawberryPrice;
+            double potatoTotalPrice = potatoGram * potatoPrice;
+            double tomatoTotalPrice = tomatoGram * tomatoPrice;
+
+            Console.WriteLine("Purchased Product: Apple - " + "Price per kilogram: " + applePrice + " - Gramaj: " + appleGram + " - Toplam Tutar: " + appleTotalPrice);
+
+            Console.WriteLine("Purchased Product: Orange - " + "Price per kilogram: " + orangePrice + " - Gramaj: " + orangeGram + " - Toplam Tutar: " + orangeTotalPrice);
+
+            Console.WriteLine("Purchased Product: Strawberry - " + "Price per kilogram: " + strawberryPrice + " - Gramaj: " + strawberryGram + " - Toplam Tutar: " + strawberryTotalPrice);
+
+            Console.WriteLine("Purchased Product: Patato - " + "Price per kilogram: " + potatoPrice + " - Gramaj: " + potatoGram + " - Toplam Tutar: " + potatoTotalPrice);
+
+            Console.WriteLine("Purchased Product: Tomato - " + "Price per kilogram: " + tomatoPrice + " - Gramaj: " + tomatoGram + " - Toplam Tutar: " + tomatoTotalPrice);
+
+            double shoppingTotalPrice = appleTotalPrice + orangeTotalPrice + strawberryTotalPrice + tomatoTotalPrice + potatoTotalPrice;
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("Total Price of shopping" + shoppingTotalPrice + " TL");
+
+            #endregion
+
+            #region CharVariables
+
+            char symbol;
+            symbol = 'a';
+
+            Console.WriteLine(symbol);
+
+            #endregion
+
+            #region KeyboardDataEntryStringVariables
+
+            Console.WriteLine("**** Passenger Information ****");
+            Console.WriteLine();
+
+            string passengerName, passengerSurname, passengerDistrict, passengerCity, passengerAge, passengerIdentityNumber;
+
+            Console.Write("Name: ");
+            passengerName = Console.ReadLine();
+
+            Console.Write("Surname: ");
+            passengerSurname = Console.ReadLine();
+
+            Console.Write("District: ");
+            passengerDistrict = Console.ReadLine();
+
+            Console.Write("City: ");
+            passengerCity = Console.ReadLine();
+
+            Console.Write("Age: ");
+            passengerAge = Console.ReadLine();
+
+            Console.Write("Yolcu TC Kimlik No: ");
+            passengerIdentityNumber = Console.ReadLine();
+
+            Console.WriteLine();
+
+            Console.WriteLine("-----------------------");
+            Console.WriteLine("Yolcu TC Kimlik No: " + passengerIdentityNumber + " - Yolcu Ad Soyad: " + passengerName + " " + passengerSurname + " " + passengerDistrict + " / " + passengerCity + " " + passengerAge);
+
+            #endregion
+
+            #region IntegerEntryandConversionsfromtheKeyboard
+
+            int shoesPrice, computerPrice, chairPrice, tvPrice;
+            shoesPrice = 1000;
+            computerPrice = 20000;
+            chairPrice = 5000;
+            tvPrice = 12000;
+
+            int shoesCount, computerCount, chairCount, tvCount;
+
+            Console.Write("Please enter the number of shoes you purchased: ");
+            shoesCount = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the number of computers you purchased: ");
+            computerCount = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the number of chairs you purchased: ");
+            chairCount = int.Parse(Console.ReadLine());
+
+            Console.Write("Please enter the number of television you purchased: ");
+            tvCount = int.Parse(Console.ReadLine());
+
+            int totalPrice = shoesCount * shoesPrice + computerCount * computerPrice + chairCount * chairPrice + tvCount * tvPrice;
+
+            Console.WriteLine();
+            Console.WriteLine("Total Price: " + totalPrice);
+
+            #endregion
+
+            #region DecimalNumberOperationsfromtheKeyboard
+
+            double exam1, exam2, exam3, result;
+
+            Console.Write("Enter your exam grade 1: ");
+            exam1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter your exam grade 1: ");
+            exam2 = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter your exam grade 1: ");
+            exam3 = double.Parse(Console.ReadLine());
+
+            result = (exam1 + exam2 + exam3) / 3;
+
+            Console.WriteLine();
+            Console.WriteLine("Your Exam Average " + result);
+
+            #endregion
+
+            #region CharacterEntryfromKeyboard
+
+            char gender;
+            Console.Write("Select Gender: ");
+            gender = char.Parse(Console.ReadLine());
+            Console.WriteLine("Selected Gender: " + gender);
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
